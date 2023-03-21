26cb6423e23836602405c934c7ae9aa1d7cb4336
Matheus <matheus_s_a@hotmail.com>
2023-03-21 19:37:26 -0400

    add azure function app

---
+++ sqlfunction/GetProduct.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using domain.Services;
+
+
+namespace sqlfunction
+{
+    public class GetProduct {
+
+        private readonly IProductService _productService;
+
+        public GetProduct(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [FunctionName("GetProduct")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+           
+            var products = _productService.GetProducts();
+            return new OkObjectResult(products);
+        }
+    }
+}
---
+++ sqlfunction/SqlFunctionsStartup.cs
@@ -0,0 +1,32 @@
+﻿using domain.Repositories;
+using domain.Services;
+using infra.Repositories;
+using infra.Services;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.FeatureManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: FunctionsStartup(typeof(sqlfunction.SqlFunctionsStartup))]
+namespace sqlfunction
+{
+    public  class SqlFunctionsStartup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddTransient<IProductService, ProductService>();
+            builder.Services.AddTransient<IProductRepository, ProductRepository>();
+            builder.Services.AddFeatureManagement();
+        }
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            var connectionString = "Endpoint=https://webapp-config.azconfig.io;Id=R41t-l0-s0:wkew/Z2SNYqotKPRu0Z5;Secret=Dy93q7PDgqCr7HKeL/HshnL2BrExCpb0pANcNksb9vQ=";
+            builder.ConfigurationBuilder.AddAzureAppConfiguration(connectionString);
+        }
+    }
+}
